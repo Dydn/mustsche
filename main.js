191a5dd78c15df824fb1aa3f34281ac1c4251f3a
DyDn <126629784+Dydn@users.noreply.github.com>
2023-10-12 16:43:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+
+  Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+
+  camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+      
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+      //add result
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+  console.log('ml5 version:', ml5.version);
+  
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/SGWTxu2zX/model.json',modelLoaded);
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+
+    gesture = results[0].label;
+    
+    toSpeak = "";
+    
+    if(gesture == "amazing")
+    {
+      toSpeak = "This is looking amazing";
+      //add result_object_gesture_icon
+      document.getElementById("").innerHTML = "&#128076;";
+    }
+    else if(gesture == "best")
+    {
+      toSpeak = "All the best";
+      //add result_object_gesture_icon
+      document.getElementById(" ").innerHTML = "&#128077;";
+    }
+    else if(gesture == "victory")
+    {
+      toSpeak = "That was the marvelous victory";
+      //add result_object_gesture_icon
+      document.getElementById(" ").innerHTML = "&#9996;";
+    }
+
+    speak();
+  }
+}
+
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = toSpeak;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+}
